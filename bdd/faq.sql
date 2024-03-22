b1a0c699e4d28385f958ab4c4d2d0be73bc180a7
Mathias <yuma82000.yuma82000@gmail.com>
2024-03-22 08:54:35 +0100

    changement bdd et ajout nouv bdd

--- bdd/faq.sql
+++ bdd/faq.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : ven. 22 mars 2024 à 08:46
+-- Généré le : mar. 19 mars 2024 à 13:34
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `appfaq`
 --
-
+CREATE DATABASE IF NOT EXISTS `appfaq` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `appfaq`;
 -- --------------------------------------------------------
 
 --
@@ -30,12 +31,22 @@ SET time_zone = "+00:00";
 CREATE TABLE `faq` (
   `id_faq` bigint(11) NOT NULL,
   `question` text NOT NULL,
-  `reponse` text DEFAULT NULL,
+  `reponse` text NOT NULL,
   `dat_question` datetime NOT NULL,
-  `dat_reponse` datetime DEFAULT NULL,
-  `id_user` bigint(11) NOT NULL,
-  `id_ligue` bigint(11) DEFAULT NULL
+  `dat_reponse` datetime NOT NULL,
+  `id_user` bigint(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+--
+-- Déchargement des données de la table `ligue`
+--
+INSERT INTO faq (question, reponse, dat_question, dat_reponse, id_user)
+VALUES
+('Comment puis-je me connecter à mon compte?', 'Pour vous connecter à votre compte, vous devez saisir votre adresse e-mail et votre mot de passe dans les champs appropriés.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1),
+('Comment puis-je modifier mes informations personnelles?', 'Pour modifier vos informations personnelles, accédez à votre profil et cliquez sur "Modifier".', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2),
+('Comment puis-je supprimer mon compte?', 'Pour supprimer votre compte, accédez à votre profil et cliquez sur "Supprimer".', CURRENT_TIMESTAMP, NULL, 3),
+('Comment puis-je signaler un problème ou un bug?', 'Pour signaler un problème ou un bug, veuillez remplir le formulaire de contact et décrire le problème ou le bug rencontré.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 2),
+('Comment puis-je me déconnecter de mon compte?', 'Pour vous déconnecter de votre compte, cliquez sur le bouton "Déconnexion" en haut à droite de la page.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 1),
+('Comment puis-je réinitialiser mon mot de passe?', 'Pour réinitialiser votre mot de passe, cliquez sur "Mot de passe oublié" lors de la connexion.', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 3);
 
 -- --------------------------------------------------------
 
@@ -81,15 +92,7 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id_user`, `pseudo`, `mdp`, `mail`, `id_usertype`, `id_ligue`) VALUES
 (1, 'aa', 'aa', 'aa@aa.net', 1, 1),
 (2, 'bb', 'bb', 'bb@bb.net', 2, 1),
-(3, 'cc', 'cc', 'cc@cc.net', 3, 5),
-(4, 'zz', '$2y$10$mDrZOKg6Z46T7Z8lBjQiMeC069eH1afsLFnDnPAhXOTf1KglGdnMG', 'zz@zz.net', 1, 1),
-(5, 'mathieu', '$2y$10$iVF4HHsRsVDBnMgE4ExQ6.lxy7ujVP8Ei1mOOrJJJBZ.xpmQ.vWF6', 'mathieu@mathieu.net', 3, 1),
-(6, 'ee', '$2y$10$Dyc3fVCG25M8BYLITD/Gae71VD/MPrrCzdKrSFZMLZzUqKfJIos7G', 'ee@ee.net', 3, 1),
-(7, 'samuel', '$2y$10$GiMDRIx4mw4Fo9JmL6ObQulX/5u3bbr1R.r4Vs5aIPdUH0HGu4EHu', 'samuel@samuel.net', 2, 1),
-(8, 'rr', 'rr', 'rr@rr.net', 3, 1),
-(9, 'nn', '$2y$10$rRu2r3uTzX29sC5KjaLPGOzW51WaWWx27Xx4ay.wLwYced.oNXqrG', 'nn@nn.net', 2, 1),
-(10, 't', '$2y$10$YQmsZNVPCb2BD2gVNWQAI.EZAfoNCw91ogC7rziYS8GmCNzecJJjS', 't@t.t', 1, 1),
-(11, 'yy', '$2y$10$akqM3VxuzGrxaZEYi10acO/gBPTtqLXO7jTC6v62bf3jNllSy3gT.', 'yy@yy.net', 1, 1);
+(3, 'cc', 'cc', 'cc@cc.net', 3, 5);
 
 -- --------------------------------------------------------
 
@@ -157,7 +160,7 @@ ALTER TABLE `faq`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id_user` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Contraintes pour les tables déchargées

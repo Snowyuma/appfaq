20db46326e87ac544bac073cebfe6c4328f96454
Mathias <yuma82000.yuma82000@gmail.com>
2024-04-01 19:25:54 +0200

    commentaire on pages

--- FAQ.php
+++ FAQ.php
@@ -1,35 +1,46 @@
 <?php
+// Démarre une session PHP pour gérer les variables de session
 session_start();
+
+// Inclut le fichier de liaison contenant la fonction de connexion à la base de données
 include "include/liaison.php";
+
+// Établit une connexion à la base de données
 $dbh = db_connect();
 
-// Vérification de l'authentification de l'utilisateur
+// Vérifie si l'utilisateur est authentifié, sinon le redirige vers la page de connexion
 if (!isset($_SESSION['id_user'])) {
     header("Location: Connexion.php");
     exit();
 }
 
+// Récupère les variables de session pertinentes pour l'utilisateur
 $id_usertype = isset($_SESSION['id_usertype']) ? $_SESSION['id_usertype'] : '';
 $id_ligue = isset($_SESSION['id_ligue']) ? $_SESSION['id_ligue'] : '';
 $id_user = isset($_SESSION['id_user']) ? $_SESSION['id_user'] : '';
+
+// Vérifie le type de ligue de l'utilisateur pour déterminer la requête SQL à exécuter
 if ($_SESSION['id_ligue'] == 5) {
+    // Requête SQL pour récupérer toutes les questions/réponses pour l'administrateur
     $sql = "SELECT * FROM faq , user, ligue 
-    where faq.id_user=user.id_user  
-    and user.id_ligue=ligue.id_ligue";
+    WHERE faq.id_user=user.id_user  
+    AND user.id_ligue=ligue.id_ligue";
     try {
         $sth = $dbh->prepare($sql);
         $sth->execute();
         $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
     } catch (PDOException $ex) {
+        // En cas d'erreur lors de l'exécution de la requête, affiche un message d'erreur et arrête le script
         die("Erreur lors de la requête SQL : " . $ex->getMessage());
     }
 } elseif ($_SESSION['id_ligue'] == 1 || $_SESSION['id_ligue'] == 2 || $_SESSION['id_ligue'] == 3 || $_SESSION['id_ligue'] == 4) {
+    // Requête SQL pour récupérer les questions/réponses spécifiques à la ligue de l'utilisateur
     $sql = "SELECT  *
-    FROM faq, user,ligue
-     WHERE faq.id_user=user.id_user 
-     and user.id_ligue=ligue.id_ligue
-      and ligue.id_ligue=:id_ligue 
-      group by faq.id_faq";
+    FROM faq, user, ligue
+    WHERE faq.id_user=user.id_user 
+    AND user.id_ligue=ligue.id_ligue
+    AND ligue.id_ligue=:id_ligue 
+    GROUP BY faq.id_faq";
     try {
         $sth = $dbh->prepare($sql);
         $sth->execute(array(
@@ -37,11 +48,12 @@ if ($_SESSION['id_ligue'] == 5) {
         ));
         $rows = $sth->fetchAll(PDO::FETCH_ASSOC);
     } catch (PDOException $ex) {
+        // En cas d'erreur lors de l'exécution de la requête, affiche un message d'erreur et arrête le script
         die("Erreur lors de la requête SQL : " . $ex->getMessage());
     }
 }
-
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -68,6 +80,7 @@ if ($_SESSION['id_ligue'] == 5) {
     <div class="formfaq">
         <form action="<?php $_SERVER["PHP_SELF"] ?>" class="sub-formfaq">
             <div class="upper-form">
+                <!-- Titre de la FAQ -->
                 <h2>FAQ de <?php echo $_SESSION["lib_ligue"] ?></h2>
             </div>
 
@@ -75,42 +88,36 @@ if ($_SESSION['id_ligue'] == 5) {
             <table>
                 <tr>
                     <!-- Entêtes de colonne -->
-
                     <th class="p">Auteur</th>
                     <th class="p">Question</th>
                     <th class="p">Réponse</th>
                     
-                    <?php if ($id_usertype == 3 || $id_usertype == 2) {
+                    <?php 
+                    // Vérifie le type d'utilisateur pour afficher les colonnes supplémentaires si nécessaire
+                    if ($id_usertype == 3 || $id_usertype == 2) {
                         echo '<th class="p">Action</th>';
                         if ($id_usertype == 3){
-                            echo '<th class="p">ligue</th>';
+                            echo '<th class="p">Ligue</th>';
                         }
                     } ?>
                 </tr>
                 <?php
-               // echo "<pre>";
-               //print_r($rows);
-               //echo "</pre>";
-                // Boucle résultats de la requête SQL et affiche chaque question et réponse dans des lignes de tableau
+                // Boucle sur les résultats de la requête SQL et affiche chaque question et réponse dans des lignes de tableau
                 foreach ($rows as $row) {
                     echo '<tr class="tr">';
                     echo '<td class="p">' . $row["pseudo"] . '</td>';
                     echo '<td class="p">' . $row["question"] . '</td>';
                     echo '<td class="p">' . $row["reponse"] . '</td>';
-                   
-
+                    
                     // Vérifie si l'utilisateur est l'administrateur ou le super administrateur pour afficher les liens de modification/suppression
-
-
-
                     if ($_SESSION['id_usertype'] == 3 || $_SESSION['id_usertype'] == 2) {
                         $id_faq = $row['id_faq'];
-
                         echo '<td><a href="Supr.php?id=' . $id_faq . '" class="action_tab">Supprimer </a><br>';
                         echo '<a href="Modif.php?id=' . $id_faq . '" class="action_tab">Modification</a>';
                         
+                        // Affiche la colonne de la ligue si l'utilisateur est un super administrateur
                         if ($_SESSION['id_usertype'] == 3){
-                             echo '<td class="p">' . $row["lib_ligue"] . '</td>';
+                            echo '<td class="p">' . $row["lib_ligue"] . '</td>';
                         }
                     }
                     echo '</tr>';
@@ -125,14 +132,14 @@ if ($_SESSION['id_ligue'] == 5) {
     <!-- Pied de page -->
     <div class="légale">
         <p>
-        <h4>Projet AP2 site N2L FAQ</h4>
-        <h5>2023-2024<br>
-            BTS SIO: Service Informatique aux Organisations<br>
-            option SLAM: Solutions Locigielles Application Metier<br>
-            Créateurs: <br>
-            Chef de projet: Mathieu Fraux <br>
-            Developpeur principal: Lucas Rauzy <br>
-            Developpeur secondaire: Colmagro Mathias </h5>
+            <h4>Projet AP2 site N2L FAQ</h4>
+            <h5>2023-2024<br>
+                BTS SIO: Service Informatique aux Organisations<br>
+                option SLAM: Solutions Logicielles Applications Métiers<br>
+                Créateurs: <br>
+                Chef de projet: Mathieu Fraux <br>
+                Développeur principal: Lucas Rauzy <br>
+                Développeur secondaire: Colmagro Mathias </h5>
         </p>
     </div>
 </body>
--- include/liaison.php
+++ include/liaison.php
@@ -2,6 +2,7 @@
 //
 // ph20b : pays de l'U.E. avec une BD
 //
+
 /**
  * Connexion à la base de données
  *
@@ -9,28 +10,35 @@
  */
 function db_connect()
 {
+  // Définition des informations de connexion à la base de données
   $dsn = 'mysql:host=localhost;dbname=appfaq';  // contient le nom du serveur et de la base
   $user = 'root';
   $password = '';
+
+  // Tentative de connexion à la base de données
   try {
     $dbh = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   } catch (PDOException $ex) {
+    // En cas d'erreur lors de la connexion, affiche un message d'erreur et arrête le script
     die("Erreur lors de la connexion SQL : " . $ex->getMessage());
   }
+  
+  // Retourne l'objet de connexion PDO
   return $dbh;
 }
-//pour la page de connexion
+
+// Fonction pour la page de connexion
 function connexion()
 {
-  //lien avec la BDD
+  // Lien avec la base de données
   $dbh = db_connect();
-  //recuperation du pseudo et du mot de passe
+  
+  // Récupération du pseudo et du mot de passe depuis le formulaire
   $pseudo = isset($_POST['pseudo']) ? $_POST['pseudo'] : '';
   $motdepasse = isset($_POST['mdp']) ? $_POST['mdp'] : '';
 
-  
-  // recuperation du pseuso dans la BDD
+  // Requête pour récupérer le pseudo depuis la base de données
   $sql_pseudo = "SELECT pseudo FROM user WHERE pseudo=:pseudo";
   try {
     $sth = $dbh->prepare($sql_pseudo);
@@ -42,9 +50,9 @@ function connexion()
     die("Erreur lors de la requête SQL : " . $ex->getMessage());
   }
   
-  //verification si lme pseudo existe
+  // Vérification si le pseudo existe
   if (count($resultat_pseudo) > 0) {
-    // verification du mot de passe
+    // Requête pour récupérer le mot de passe correspondant au pseudo
     $sql_mdp = "SELECT mdp FROM user WHERE pseudo=:pseudo";
     try {
       $sth = $dbh->prepare($sql_mdp);
@@ -56,9 +64,9 @@ function connexion()
       die("Erreur lors de la requête SQL : " . $ex->getMessage());
     }
     
-    //verification si le mot de passe est correct
+    // Vérification si le mot de passe est correct
     if ($resultat_mdp && password_verify($motdepasse, $resultat_mdp['mdp'])) {
-      //recuperation du type d'utilisateur 
+      // Requête pour récupérer le type d'utilisateur
       $sqlid_usertype = "SELECT id_usertype FROM user WHERE pseudo=:pseudo";
       try {
         $sth = $dbh->prepare($sqlid_usertype);
@@ -70,7 +78,7 @@ function connexion()
         die("Erreur lors de la requête SQL : " . $ex->getMessage());
       }
       
-      //recuperation de la ligue
+      // Requête pour récupérer l'id de la ligue
       $sql_ligue = "SELECT id_ligue FROM user WHERE pseudo=:pseudo";
       try {
         $sth = $dbh->prepare($sql_ligue);
@@ -81,7 +89,8 @@ function connexion()
       } catch (PDOException $ex) {
         die("Erreur lors de la requête SQL : " . $ex->getMessage());
       }
-      //recuperation de l'id utilisateur
+      
+      // Requête pour récupérer l'id de l'utilisateur
       $sql_user = "SELECT id_user FROM user WHERE pseudo=:pseudo";
       try {
         $sth = $dbh->prepare($sql_user);
@@ -93,9 +102,8 @@ function connexion()
         die("Erreur lors de la requête SQL : " . $ex->getMessage());
       }
 
-      
-      // recuperation de libellé de la ligue
-      $sql_lib_ligue = "SELECT lib_ligue FROM user inner join ligue on user.id_ligue=ligue.id_ligue WHERE user.pseudo=:pseudo";
+      // Requête pour récupérer le libellé de la ligue
+      $sql_lib_ligue = "SELECT lib_ligue FROM user INNER JOIN ligue ON user.id_ligue=ligue.id_ligue WHERE user.pseudo=:pseudo";
       try {
         $sth = $dbh->prepare($sql_lib_ligue);
         $sth->execute(array(
@@ -106,19 +114,26 @@ function connexion()
         die("Erreur lors de la requête SQL : " . $ex->getMessage());
       }
 
+      // Affichage d'un message de connexion réussie
       echo "<p>Connecté</p>";
     
-      $_SESSION['pseudo'] = implode ($resultat_pseudo);
-      $_SESSION['id_usertype'] = implode ($resultat_id_usertype);
-      $_SESSION['id_ligue'] = implode ($resultat_id_ligue);
-      $_SESSION['id_user'] = implode ($resultat_id_user);
-      $_SESSION['lib_ligue'] = implode ($resultat_lib_ligue);
+      // Définition des variables de session
+      $_SESSION['pseudo'] = implode($resultat_pseudo);
+      $_SESSION['id_usertype'] = implode($resultat_id_usertype);
+      $_SESSION['id_ligue'] = implode($resultat_id_ligue);
+      $_SESSION['id_user'] = implode($resultat_id_user);
+      $_SESSION['lib_ligue'] = implode($resultat_lib_ligue);
+      
+      // Redirection vers la page FAQ après la connexion
       header("Location: FAQ.php");
       exit();
     } else {
-      echo "mot de passe incorrect";
+      // Affichage d'un message en cas de mot de passe incorrect
+      echo "Mot de passe incorrect";
     }
   } else {
+    // Affichage d'un message en cas d'identifiant inconnu
     echo "Identifiant inconnu !";
   }
 }
+?>
